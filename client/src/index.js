95774f0e7dab2c685e53c2ee502f654601871bd1
Christen Ford <christen0ford@gmail.com>
2021-01-30 21:28:32 -0500

    rebase to vanilla js because it is 10000 times better

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './frontpage';
-
-ReactDOM.render(
-<App />,
-document.getElementById('root')
-);
---
+++ client2/js/index.js
@@ -0,0 +1,42 @@
+/**
+ * Requests data for the chart in an asynchrounous fashion.
+ * Use the `await requestChartData().then(data => {});` pattern when calling this function.
+ * :param ticker: The stock ticker to lookup.
+ * :param start_date: The start date of the lookup window.
+ * :param end_date: The end date of the lookup window.
+ * Returns the JSON data for the stock ticker gathered by the API server.
+ */
+// ticker: str, start_date: str, stop_date: str
+async function requestChartData(ticker, start_date, stop_date, interval) {
+    var api_str = "http://localhost:8000";
+    
+    var params = new URLSearchParams();
+    params.set('ticker', ticker);
+    params.set('start_date', start_date);
+    params.set('stop_date', stop_date);
+    params.set('interval', interval);
+    const redditData = await fetch(new URL(api_str + "/reddit?" + params.toString()), {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin'
+    });
+
+    var params = new URLSearchParams();
+    params.set('ticker', ticker);
+    params.set('start_date', start_date);
+    params.set('stop_date', stop_date);
+    params.set('interval', interval);
+    const marketData = await fetch(new URL(api_str + "/market?" + params.toString()), {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin'
+    });
+    
+    return {"marketData": marketData.json(), "redditData": redditData.json()};
+}
+
+function intervalClickHandler() {
+
+}
